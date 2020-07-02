3a24c9ba0dec80f9ba743d5fb1109aabc95dd595
aalacy <aaronlacywhite387@gmail.com>
2020-07-01 23:36:58 -0700

    change api

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 // const api = "https://hearbk-server-api.herokuapp.com";
-const api = "http://localhost:8000/api/quoteshell/";
-// const api = "https://hearbk-server.herokuapp.com";
+// const api = "http://localhost:8000/api/quoteshell/";
+const api = "https://api-ninja.herokuapp.com/api/quoteshell/";
 export const genericHeaders = () => ({
   "Content-Type": "application/json",
   Accept: "application/json"
